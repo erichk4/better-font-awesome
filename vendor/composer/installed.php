245a01036d54194fa15fbebc01c65d6b50ce611d
wuchtl@gmail.com <wuchtl@gmail.com>
2022-08-29 18:45:13 +0200

    Add vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mickey-kay/better-font-awesome',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e506f91f7800e302670316f2d7c2b2d0dfddee7d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mickey-kay/better-font-awesome' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e506f91f7800e302670316f2d7c2b2d0dfddee7d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mickey-kay/better-font-awesome-library' => array(
+            'pretty_version' => '2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => 'f9e4bb32c4d7c02d0e44a473873fddd40feefebf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mickey-kay/better-font-awesome-library',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
